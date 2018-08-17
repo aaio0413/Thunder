b3ded24f89d3f09ea0252226d4a936e0ebbd0dbf
AAIO1992 <takahiro04kawaguchi13@gmail.com>
2018-08-17 18:46:15 +0900

    Added folders and files needed. There are three folders in app folder. Data, Public, and Routing folders. Each folder has file/s to take roles associated with the folder's name. To do: function when user submits survey, compare survey's score and bring some bata back to user. deploy this to heroku to utilize surver.

---
+++ app/data/members.js
@@ -0,0 +1,29 @@
+const membersList = [
+    {
+      customerName: "Takahiro",
+      photoUrl: "https://www.google.co.jp/url?sa=i&source=images&cd=&cad=rja&uact=8&ved=2ahUKEwiA_tru2PPcAhUL7mEKHUZVBHEQjRx6BAgBEAU&url=https%3A%2F%2Fwww.thehindu.com%2Fthread%2Freflections%2Fa-meme-is-a-loaded-gun-so-how-about-meme-control-laws%2Farticle19587944.ece&psig=AOvVaw1XMOANcJEEvoIrC1uUbpLm&ust=1534581769175355",
+      scores: [
+          '3',
+          '3',
+          '3',
+          '3',
+          '3',
+          '3',
+          '3',
+          '3',
+          '3',
+          '3'
+      ]
+    }
+];
+
+
+
+
+
+
+
+
+
+
+module.exports = membersList;
---
+++ server.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const path = require("path");
+
+const app = express();
+const PORT = process.env.PORT || 8080;
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+
+
+require("./app/routing/apiRoutes")(app); // ---------- Routers -------------//
+require("./app/routing/htmlRoutes")(app);
+
+
+
+
+
+
+
+
+
+
+app.listen(PORT, function() {
+    console.log("App listening on PORT: " + PORT);
+});
